2183ce0e97ec8fbd1165a7c6ab1e02cf63095e82
vvaufrey <vvaufrey@kaliop.com>
2021-09-24 16:02:51 +0200

    Update

--- public/index.php
+++ public/index.php
@@ -1,65 +1,48 @@
 <?php
 
-use App\App;
-use App\Entity\CommentEntity;
-use App\Entity\UserEntity;
-use App\Models\CommentModel;
-use App\Models\UserModel;
+// @TODO securise public folder. (droits fichier)
+
+use App\Controllers\ErrorController;
+use Core\Acl\ACL;
 use Core\Configuration\Config;
+use Core\Controllers\Controller;
 use Core\Router\Router;
 use Core\Router\RouterException;
+use Core\Utils\Request;
 
-define('ROOT', __DIR__);
+define('ROOT', __DIR__ .  '/..');
 
 // Start session
 session_start();
 
 // Require autoload
-require __DIR__ . '/vendor/autoload.php';
-require __DIR__ . '/app/App.php';
-require __DIR__ . '/Core/Exception/Exception.php';
-
-// Get config
-$config = new Config();
-$url = $config->getConfig(Config::SECURE_URL_CONFIG_CODE) ?
-            $config->getConfig(Config::SECURE_URL_CONFIG_CODE) :
-            $config->getConfig(Config::UNSECURE_URL_CONFIG_CODE);
-
-
+require '../vendor/autoload.php';
+require '../app/App.php';
+require '../Core/Exception/Exception.php';
 
 // Maintenance
+$config = new Config();
 $maintenanceMode = $config->getConfig(Config::MAINTENANCE_MODE_CONFIG_CODE) == 1;
 if ($maintenanceMode) {
-    // Maintenance
+    return new ErrorController('maintenance');
 }
-
-$app = new App();
-$app->app();
-
-$userModel = new UserModel('user');
-$collection = $userModel
-    ->addAttributToFilter('name', 'Vince')
-    ->addAttributToFilter('id', ['>', 0])
-    ->addAttributToFilter('email', ['like', '%vince%'])
-    ->getCollection();
-
-
-foreach ($collection as $user) {
-    $user->setName('viince');
-    $user->save();
-    $user->delete();
-}
-exit;
 
 // Set router URL
-$router = new Router($url);
+$request = new Request();
+$router = new Router($request->getUrl());
 
 /**
  * Index route
- * @GET & @POST route
+ * @GET METHOD
  */
-$router->get('/test', function () {});
-$router->post('/test', function () {});
+$router->get('/index', null, ACL::EVERYONE);
+
+/**
+ * Index route
+ * @GET & @POST METHOD
+ */
+$router->get('/test', null);
+$router->post('/test', null);
 
 // Run router
 try {
